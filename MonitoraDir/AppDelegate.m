1b7eccc8cf0e444ad4477636ecca57b53b36e888
Danilo Altheman <danilo@iMac.local>
2012-10-13 11:37:30 -0300

    Código comentado

--- MonitoraDir/AppDelegate.m
+++ MonitoraDir/AppDelegate.m
@@ -7,19 +7,13 @@
 //
 
 #import "AppDelegate.h"
-void file_events_callback(ConstFSEventStreamRef streamRef,
-                       void *userData,
-                       size_t numEvents,
-                       void *eventPaths,
-                       const FSEventStreamEventFlags eventFlags[],
-                       const FSEventStreamEventId eventIds[]) {
+void file_events_callback(ConstFSEventStreamRef streamRef, void *userData, size_t numEvents, void *eventPaths, const FSEventStreamEventFlags eventFlags[], const FSEventStreamEventId eventIds[]) {
     
     char **paths = eventPaths;
     
-    
     for (int  i = 0; i < numEvents; i++) {
         FSEventStreamEventFlags flags = eventFlags[i];
-        NSLog(@"num events: %zi, %llu, %02x, %@", numEvents,eventIds[i], flags, [NSString stringWithUTF8String:paths[i]]);
+        NSLog(@"Event: %llu, Flags: %02x, Path: %@", eventIds[i], flags, [NSString stringWithUTF8String:paths[i]]);
         if (flags & kFSEventStreamEventFlagItemCreated) {
             NSLog(@"Criou");
         }
@@ -95,18 +89,26 @@ Denotes a special event sent when a volume is unmounted underneath one of the pa
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Diretório a ser monitorado.
     CFStringRef path = CFSTR("/Users/danilo/Desktop/DroBoh");
+    // Cria o array com a lista de diretórios.
     CFArrayRef pathsToWatch = CFArrayCreate(NULL, (const void **)&path, 1, NULL);
+    // Cria a estrutura para o context.
     streamContext = (FSEventStreamContext *)malloc(sizeof(FSEventStreamContext));
     streamContext->version = 0;
     streamContext->info = NULL;
     streamContext->retain = NULL;
     streamContext->release = NULL;
     streamContext->copyDescription = NULL;
-    streamRef = FSEventStreamCreate(NULL, &file_events_callback, streamContext, pathsToWatch, kFSEventStreamEventIdSinceNow, 1.0, kFSEventStreamEventFlagNone);
-    FSEventStreamScheduleWithRunLoop(streamRef, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
-    FSEventStreamStart(streamRef);
     
+    // Verifica por alterações à cada 1/2s
+    CFTimeInterval latency = 0.5;
+    // Cria a Stream, e define a função de callback.
+    streamRef = FSEventStreamCreate(NULL, &file_events_callback, streamContext, pathsToWatch, kFSEventStreamEventIdSinceNow, latency, kFSEventStreamEventFlagNone);
+    // Coloca no Runloop
+    FSEventStreamScheduleWithRunLoop(streamRef, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+    // Inicia o monitoramento.
+    FSEventStreamStart(streamRef);
 }
 
 -(NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender {
