07f3c5ff316de06845162dd8703878a7ea7a7255
Danilo Altheman <danilo@iMac.local>
2012-10-12 21:17:20 -0300

    Initial Commit

---
+++ MonitoraDir/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MonitoraDir
+//
+//  Created by Danilo Altheman on 12/10/12.
+//  Copyright (c) 2012 Danilo Altheman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MonitoraDir/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  MonitoraDir
+//
+//  Created by Danilo Altheman on 12/10/12.
+//  Copyright (c) 2012 Danilo Altheman. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
